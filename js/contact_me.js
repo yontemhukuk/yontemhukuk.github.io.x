6dc8716e560142d09db5610381a31bca70f1c1af
yontemhukuk <avbaran11@gmail.com>
2015-08-30 21:15:41 +0300

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -48,7 +48,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Messajiniz gonderildi. </strong>");
+                        .append("<strong>Mesajınız Gönderildi. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
